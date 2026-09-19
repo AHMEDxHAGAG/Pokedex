575a596e54144612f0eabd737ad9e3fd9e2eabac
AHMEDxHAGAG <ahmedheagag23@gmail.com>
2026-09-19 12:02:00 +0300

    Caching the Location Areas API Results in the Memory

--- internal/commands/commands_map.go
+++ internal/commands/commands_map.go
@@ -4,6 +4,7 @@ package commands
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -18,17 +19,35 @@ var locationArea struct {
 	Results []result `json:"results"`
 }
 
+/*
+	TODO:
+	Update your code that makes requests to the PokeAPI to use the cache.
+	Create the cache once and reuse it in your PokeAPI request layer.
+	If you already have the data for a given URL (which is our cache key) in the cache,
+	you should use that instead of making a new request.
+	Whenever you do make a request,
+	you should add the response to the cache.
+*/
+
 func commandMap(conf *Config) error {
 	if conf.NextMap == "null" {
 		fmt.Println("No More Location Areas")
 		return nil
 	}
-	req, err := http.Get(conf.NextMap)
-	if err != nil {
-		return err
+	val, found := conf.Cache.Get(conf.NextMap)
+	if found {
+	} else {
+		req, err := http.Get(conf.NextMap)
+		if err != nil {
+			return err
+		}
+		defer func() { _ = req.Body.Close() }()
+		val, err = io.ReadAll(req.Body)
+		if err != nil {
+			return err
+		}
 	}
-	defer func() { _ = req.Body.Close() }()
-	if err := json.NewDecoder(req.Body).Decode(&locationArea); err != nil {
+	if err := json.Unmarshal(val, &locationArea); err != nil {
 		return err
 	}
 	conf.NextMap = locationArea.Next
@@ -45,12 +64,20 @@ func commandMapb(conf *Config) error {
 		fmt.Println("There is No Previous Location Areas")
 		return nil
 	}
-	req, err := http.Get(conf.PrevMap)
-	if err != nil {
-		return err
+	val, found := conf.Cache.Get(conf.PrevMap)
+	if found {
+	} else {
+		req, err := http.Get(conf.PrevMap)
+		if err != nil {
+			return err
+		}
+		defer func() { _ = req.Body.Close() }()
+		val, err = io.ReadAll(req.Body)
+		if err != nil {
+			return err
+		}
 	}
-	defer func() { _ = req.Body.Close() }()
-	if err := json.NewDecoder(req.Body).Decode(&locationArea); err != nil {
+	if err := json.Unmarshal(val, &locationArea); err != nil {
 		return err
 	}
 	conf.NextMap = locationArea.Next
--- internal/commands/config.go
+++ internal/commands/config.go
@@ -1,12 +1,22 @@
 // Package commands
 package commands
 
+import (
+	"github.com/AHMEDxHAGAG/Pokedex/internal/pokecache"
+)
+
 type Config struct {
 	Reg     registry
 	NextMap string
 	PrevMap string
+	Cache   *pokecache.Cache
 }
 
 func NewConfig() *Config {
-	return &Config{Reg: getRegistry(), NextMap: "https://pokeapi.co/api/v2/location-area/", PrevMap: "null"}
+	return &Config{
+		Reg:     getRegistry(),
+		NextMap: "https://pokeapi.co/api/v2/location-area/",
+		PrevMap: "null",
+		Cache:   pokecache.NewCache(5),
+	}
 }
